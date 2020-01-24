17a7a05864ef25741a3e66c805b484c80da81dd3
Nazym Guliyev <guliyev.n.d@gmail.com>
2020-01-24 14:26:52 +0600

    finished step 5

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import readlineSync from 'readline-sync';
 const getName = () => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello ${name} !`);
+  return name;
 };
 
-export default getName;
+const checkEvenOdd = (randomNmb) => {
+  const EvenOdd = randomNmb % 2 === 0 ? 'yes' : 'no';
+  return EvenOdd;
+};
+export { checkEvenOdd, getName };
