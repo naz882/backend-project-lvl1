d59e6f5d81a04478b64aab6f342b087c07f19350
Nazym Guliyev <guliyev.n.d@gmail.com>
2020-01-30 14:05:21 +0600

    fix function games menu

--- src/games-menu.js
+++ src/games-menu.js
@@ -6,16 +6,15 @@ import brainGcd from './bin/games/brain-gcd';
 import brainArithsequence from './bin/games/brain-arithsequence';
 import brainPrime from './bin/games/brain-prime';
 
-console.log('Welcome to the Brain Games!');
-console.log('Please chose a game typing from 1 to 5');
-console.log('1. Even game');
-console.log('2. Brain calc');
-console.log('3. Find GCD');
-console.log('4. Arithmetic Sequence');
-console.log('5. Is Prime number?');
-const game = readlineSync.question('Your choice: ');
-
 export default () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Please chose a game typing from 1 to 5');
+  console.log('1. Even game');
+  console.log('2. Brain calc');
+  console.log('3. Find GCD');
+  console.log('4. Arithmetic Sequence');
+  console.log('5. Is Prime number?');
+  const game = readlineSync.question('Your choice: ');
   switch (game) {
     case '1':
       brainEven();
