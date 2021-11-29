313516a40be0c6f0143ccec6a4baf3a66190bb60
naz882 <guliyev.n.d@gmail.com>
2021-11-29 15:51:05 +0600

    fixed typos

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,6 +4,5 @@ import toWelcome from '../src/cli.js';
 import brainEven from '../src/games/brain-even.js';
 
 const name = toWelcome();
-//console.log(`Hello, ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 brainEven(name);
