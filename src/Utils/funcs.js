25fe9dda8cb3d1c66764da5c1e9b8ec2339a9e02
naz882 <guliyev.n.d@gmail.com>
2021-11-29 13:55:40 +0600

    make default function

--- src/Utils/funcs.js
+++ src/Utils/funcs.js
@@ -1,4 +1,4 @@
-export const getRandomIntInclusive = (min, max) => {
+export default getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
