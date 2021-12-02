5e6b6db4785ea257f8ad9d036c9dc1913d09e715
naz882 <guliyev.n.d@gmail.com>
2021-12-03 00:15:14 +0600

    brain-prime logyc change

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -21,19 +21,22 @@ const getAnswer = (bool) => {
 };
 
 export default (name) => {
-  let myAnswers = 0;
-  while (myAnswers < 3) {
+  const correctAnswer = 3;
+  let myCorrectAnswer = 0;
+  while (myCorrectAnswer < correctAnswer){
     const numb = getRandomIntInclusive(1, 40);
     const rightAnswer = isPrime(numb);
     console.log(`Question: ${numb}`);
     const answer = readlineSync.question('Your answer: ').toLowerCase();
     if (answer === getAnswer(rightAnswer)) {
       console.log('Correct!');
+      myCorrectAnswer += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
+      break;
     }
-    myAnswers += 1;
+
   }
   console.log(`Congratulations, ${name}!`);
 };
