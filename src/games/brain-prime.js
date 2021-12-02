7931c25d1360090944b049544184f7823d9d6278
naz882 <guliyev.n.d@gmail.com>
2021-12-03 01:02:23 +0600

    brain-prime logyc change

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -35,7 +35,6 @@ export default (name) => {
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      break;
     }
 
   }
