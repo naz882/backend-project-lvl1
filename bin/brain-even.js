a6c0c6bc85536e08ab1933ea630d81af7ae7f39a
naz882 <guliyev.n.d@gmail.com>
2021-12-02 22:13:33 +0600

    fixed typos

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,5 +4,5 @@ import toWelcome from '../src/cli.js';
 import brainEven from '../src/games/brain-even.js';
 
 const name = toWelcome();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 brainEven(name);
