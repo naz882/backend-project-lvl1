563fa45f003f609eb950d749f7783e269f570748
naz882 <guliyev.n.d@gmail.com>
2021-12-03 00:44:59 +0600

    brain-prime logyc change

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -34,7 +34,7 @@ export default (name) => {
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      break;
+      myCorrectAnswer += 1;
     }
 
   }
