e488c929845a4651dd84a99d3f2a9fda8d77f423
naz882 <guliyev.n.d@gmail.com>
2021-12-11 18:59:43 +0600

    fixed typos

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,7 +5,7 @@ const isPrime = (numb) => {
   if (numb <= 2) {
     return true;
   }
-  for (let i = 2; i < numb; i += 1) {
+  for (let i = 3; i < numb; i += 1) {
     if (numb % i === 0) {
       return false;
     }
